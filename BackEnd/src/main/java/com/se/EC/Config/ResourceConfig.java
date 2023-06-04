547adc6400ebe5e7751be13e33c17d868ee26c9a
Luoyue2002 <104545011+Luoyue2002@users.noreply.github.com>
2023-06-04 15:00:58 +0800

    order and shop part of backend

---
+++ BackEnd/src/main/java/com/se/EC/Config/ResourceConfig.java
@@ -0,0 +1,21 @@
+package com.se.EC.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ResourceConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //映射static路径的请求到static目录下
+        // 静态资源访问路径和存放路径配置
+                //通过image访问本地的图片
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:D:\\SE_PROGECT\\BackEnd\\src\\main\\resources\\static\\image\\");
+    }
+
+}
